a7515bb881838cc5f1aec105b04d50cc9a4b92b2
pierredeneuville <deneuville.pierre@gmail.com>
2021-12-07 16:54:32 +0100

    song at click on profile picture

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const profileImg = document.querySelector('#avatar - header');
+const profileImg = document.querySelector('#avatar-header');
 const leek = new Audio('leek.mp4');
 
 profileImg.addEventListener('click', (event) =>{
